40eee66128011b24700667f924c2094fa6fe1853
Rohit Rai <rohit0235@gmail.com>
2025-05-19 17:02:47 +0530

    function
    function

---
+++ BasicPython/breakcontinue.py
@@ -0,0 +1,10 @@
+car = ["toyota","kia","tata","mahindra"]
+for i in car:
+    
+    if(i=="kia"):
+        continue
+    print(i)
+    
+    if(i=="tata"):
+        break
+    
---
+++ BasicPython/function.py
@@ -0,0 +1,19 @@
+# function to multiply the number it is always after calling the function
+def multiply(a,b):
+    i=a*b
+    print(i)
+
+# pass is used to declare the function only
+def add(a,b):
+    pass
+
+
+a=3
+b=5
+multiply(a,b)
+add(a,b)
+
+
+
+
+
